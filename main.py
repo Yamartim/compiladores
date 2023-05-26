210b474e4f4b13ce8dd7b2c4fa6348c398d9391e
fernando-oliv <fernandooliveira@estudante.ufscar.br>
2023-05-26 13:57:59 -0300

    mudanças na main para adequar a saída solicitada

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ from AlgumaLexer import AlgumaLexer
 
 
 def main(argv):
-    #input = FileStream(argv[1], encoding='utf-8')
-    input = FileStream('input.txt', encoding='utf-8')
+    input = FileStream(argv[1], encoding='utf-8')
+    #input = FileStream('input.txt', encoding='utf-8')
     #file_str = open('input.txt','r').read()
+    file_out = open('saidaProduzida/saida_t1/'+ argv[1][:-4] + '_saida.txt', 'w')
     lexer = AlgumaLexer(input)
 
     t = lexer.nextToken()
     while (t.type != Token.EOF) :
         tokenTypeTemp = lexer.symbolicNames[t.type]
         if tokenTypeTemp == 'PalavraChave':
-            print('<\'' + t.text + '\',\'' + t.text + '\'>')
+            file_out.write('<\'' + t.text + '\',\'' + t.text + '\'>\n')
+            #print('<\'' + t.text + '\',\'' + t.text + '\'>')
         else:
-            print('<\'' + t.text + '\',' + tokenTypeTemp + '>')
+            file_out.write('<\'' + t.text + '\',\'' + tokenTypeTemp + '\'>\n')
+            #print('<\'' + t.text + '\',' + tokenTypeTemp + '>')
         t = lexer.nextToken()
     
 if __name__ == '__main__':

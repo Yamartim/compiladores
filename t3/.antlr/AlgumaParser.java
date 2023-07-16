c885d155a26460332a1fa4ef3ccba079406c073d
fernando <fernandooliveira@estudante.ufscar.br>
2023-07-16 13:02:45 -0300

    .

--- t3/.antlr/AlgumaParser.java
+++ t3/.antlr/AlgumaParser.java
@@ -1,4 +1,4 @@
-// Generated from /home/fernando/git/compiladores/t2/Alguma.g4 by ANTLR 4.9.2
+// Generated from /home/fernando/git/compiladores/t3/Alguma.g4 by ANTLR 4.9.2
 import org.antlr.v4.runtime.atn.*;
 import org.antlr.v4.runtime.dfa.DFA;
 import org.antlr.v4.runtime.*;

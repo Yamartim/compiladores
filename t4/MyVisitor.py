f905a7fc30acc6e9dad898dfa9f496481b880e3c
Yamartim <54243954+Yamartim@users.noreply.github.com>
2023-08-08 09:22:50 -0300

    adicionando regioes no visitor

--- t4/MyVisitor.py
+++ t4/MyVisitor.py
@@ -24,6 +24,7 @@ class AlgumaVisitor(ParseTreeVisitor):
         print(ctx.declaracoes())
         return self.visitChildren(ctx)
 
+#region metodos nao usados 1
 
     # Visit a parse tree produced by AlgumaParser#declaracoes.
     def visitDeclaracoes(self, ctx:AlgumaParser.DeclaracoesContext):
@@ -34,6 +35,7 @@ class AlgumaVisitor(ParseTreeVisitor):
     def visitDecl_local_global(self, ctx:AlgumaParser.Decl_local_globalContext):
         return self.visitChildren(ctx)
 
+#endregion
 
     # Visit a parse tree produced by AlgumaParser#declaracao_local.
     def visitDeclaracao_local(self, ctx:AlgumaParser.Declaracao_localContext):
@@ -53,9 +55,9 @@ class AlgumaVisitor(ParseTreeVisitor):
                 t = ctx.variavel().tipo().tipo_estendido().getText()
                 if t[0] == '^':
                     ponteiro = '&'
-                if ctx.variavel().tipo().tipo_estendido().tipo_basico_ident().IDENT() != None:
+                if ctx.variavel().tipo().tipo_estendido().tipo_basico_ident().IDENT() is not None:
                     tipoVar = ctx.variavel().tipo().tipo_estendido().tipo_basico_ident().IDENT()
-                    if tipoVar != None:
+                    if tipoVar is not None:
                         tipoVar = tipoVar.getSymbol()
 
                         if not TabelaDeSimbolos.existe(tipoVar.text):
@@ -79,6 +81,7 @@ class AlgumaVisitor(ParseTreeVisitor):
 
         return self.visitChildren(ctx)
 
+#region metodos nao usados 2
 
     # Visit a parse tree produced by AlgumaParser#variavel.
     def visitVariavel(self, ctx:AlgumaParser.VariavelContext):
@@ -149,6 +152,7 @@ class AlgumaVisitor(ParseTreeVisitor):
     def visitCmd(self, ctx:AlgumaParser.CmdContext):
         return self.visitChildren(ctx)
 
+#endregion
 
     # Visit a parse tree produced by AlgumaParser#cmdLeia.
     def visitCmdLeia(self, ctx:AlgumaParser.CmdLeiaContext):
@@ -160,8 +164,6 @@ class AlgumaVisitor(ParseTreeVisitor):
 
     # Visit a parse tree produced by AlgumaParser#cmdEscreva.
     def visitCmdEscreva(self, ctx:AlgumaParser.CmdEscrevaContext):
-
-
         return self.visitChildren(ctx)
 
     def verificarIdentVariavel(self, ctx):
@@ -172,6 +174,8 @@ class AlgumaVisitor(ParseTreeVisitor):
                 listaErros.adicionarErroSemantico(t,  'identificador ' + t.text + ' nao declarado')
             i += 1
         return
+
+#region metodos nao usados 3
 
     # Visit a parse tree produced by AlgumaParser#cmdSe.
     def visitCmdSe(self, ctx:AlgumaParser.CmdSeContext):
@@ -197,6 +201,7 @@ class AlgumaVisitor(ParseTreeVisitor):
     def visitCmdFaca(self, ctx:AlgumaParser.CmdFacaContext):
         return self.visitChildren(ctx)
 
+#endregion
 
     # Visit a parse tree produced by AlgumaParser#cmdAtribuicao.
     def visitCmdAtribuicao(self, ctx:AlgumaParser.CmdAtribuicaoContext):
@@ -306,6 +311,7 @@ class AlgumaVisitor(ParseTreeVisitor):
         return tipo
         
 
+#region metodos nao usados 3
 
     # Visit a parse tree produced by AlgumaParser#cmdChamada.
     def visitCmdChamada(self, ctx:AlgumaParser.CmdChamadaContext):
@@ -377,6 +383,7 @@ class AlgumaVisitor(ParseTreeVisitor):
     def visitParcela(self, ctx:AlgumaParser.ParcelaContext):
         return self.visitChildren(ctx)
 
+#endregion
 
     # Visit a parse tree produced by AlgumaParser#parcela_unario.
     def visitParcela_unario(self, ctx:AlgumaParser.Parcela_unarioContext):
@@ -386,6 +393,7 @@ class AlgumaVisitor(ParseTreeVisitor):
                 listaErros.adicionarErroSemantico(t,  'identificador ' + t.text + ' nao declarado')
         return self.visitChildren(ctx)
 
+#region metodos nao usados 4
 
     # Visit a parse tree produced by AlgumaParser#parcela_nao_unario.
     def visitParcela_nao_unario(self, ctx:AlgumaParser.Parcela_nao_unarioContext):
@@ -431,6 +439,6 @@ class AlgumaVisitor(ParseTreeVisitor):
     def visitOp_logico_2(self, ctx:AlgumaParser.Op_logico_2Context):
         return self.visitChildren(ctx)
 
-
+#endregion
 
 del AlgumaParser
